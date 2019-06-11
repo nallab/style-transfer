8eee45e83887952c5b7e791632b1934c08e9509c
西山昴志 <e145735@ie.u-ryukyu.ac.jp>
2019-06-11 19:29:07 +0900

    add notify cmd

---
+++ lib/slack-notify-cmd/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	name := "報告者"
+	text := "Hello from go"
+	channel := os.Getenv("CHANNEL")
+
+	jsonStr := `{"channel":"` + channel + `","username":"` + name + `","text":"` + text + `","icon_emoji":":ghost:"}"`
+
+	req, err := http.NewRequest(
+		"POST",
+		os.Getenv("WEBHOOK"),
+		bytes.NewBuffer([]byte(jsonStr)),
+	)
+
+	fmt.Println(jsonStr)
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Print(resp)
+	defer resp.Body.Close()
+}
